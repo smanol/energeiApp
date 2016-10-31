54022b32d641cef343f831101969f481c62f3ce0
Stamatis manoliadis <Stamatis manoliadis>
2016-10-31 22:57:35 +0200

    Initial commit

---
+++ app/src/main/java/com/example/smano/app/Metrhsh.java
@@ -0,0 +1,50 @@
+package com.example.smano.app;
+
+/**
+ * Created by smano on 29/10/2016.
+ */
+
+public class Metrhsh {
+
+    private String hmera;
+
+    private double kilovatora;
+    private int image ;
+
+
+
+    public Metrhsh (String hmera, double kilovatora)
+    {
+        this.hmera=hmera;
+        this.kilovatora=kilovatora;
+        if (kilovatora<= 13){
+            image = R.drawable.yes;
+        }
+        else {
+            image=R.drawable.not;
+        }
+
+    }
+
+    public double getKilovatora(){
+        return kilovatora;
+    }
+
+    public String getHmera(){
+        return hmera;
+    }
+
+
+    public int getImage(){
+        return image;
+    }
+
+    public void setHmera(String Hmera){
+    this.hmera = Hmera;
+    }
+    public void setKilovatora(double kilovatora)
+    {
+        this.kilovatora=kilovatora;
+    }
+
+}
---
+++ app/src/main/java/com/example/smano/app/MetrhshAdapter.java
@@ -0,0 +1,17 @@
+package com.example.smano.app;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+public class MetrhshAdapter {
+
+
+
+
+}
