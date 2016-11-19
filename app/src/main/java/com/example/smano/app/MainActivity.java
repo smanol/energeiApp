e8bf5881fc7016a6a1420ed0eac0a89aeeb6498b
Georgios.Manoliadis <georgemanoliadi@hotmail.com>
2016-11-19 21:27:38 +0200

    Addition of Database connection implementation with Firebase DB.

--- app/src/main/java/com/example/smano/app/MainActivity.java
+++ app/src/main/java/com/example/smano/app/MainActivity.java
@@ -12,6 +12,17 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
@@ -21,17 +32,80 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
     BarChart barChar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Firebase.setAndroidContext(this);
+        // Connect to the Firebase database
+        Firebase myDB = new Firebase("https://energieapp-6e34c.firebaseio.com/");
+        // Writing data to the database
+        //myDB.child("AgnostosAgnostou").setValue("Do you have data? You'll love Firebase.");
+        myDB.child("AgnostosAgnostou").addValueEventListener(new com.firebase.client.ValueEventListener() {
+            @Override
+            public void onDataChange(com.firebase.client.DataSnapshot dataSnapshot) {
+                System.out.println("Athena's data: "+dataSnapshot.getValue());
+            }
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+            }
+        });
 
-        barChar = (BarChart) findViewById(R.id.bargraph);
 
+        ArrayList<Metrhsh> metrhshes = new ArrayList<>();
+        metrhshes.add(new Metrhsh("13 Noemvriou", 14));
+        metrhshes.add(new Metrhsh("14 Noemvriou", 4));
+        metrhshes.add(new Metrhsh("15 Noemvriou", 3));
+        metrhshes.add(new Metrhsh("16 Noemvriou", 0.1));
+        metrhshes.add(new Metrhsh("17 Noemvriou", 14));
+        metrhshes.add(new Metrhsh("18 Noemvriou", 4));
+
+
+
+
+        //Α ρε adapter...
+        ArrayAdapter<Metrhsh> adapter = new MetrhshArrayAdapter(this, 0, metrhshes);
+
+
+
+        ListView lv;
+        lv = (ListView)findViewById(R.id.lv);
+        View footer = ((LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.footer, null, false);
+
+
+        lv.addFooterView(footer, null, false);
+        lv.setAdapter(adapter);
+
+
+        barChar= (BarChart) footer.findViewById(R.id.bargraph);
+
+        // Εδω βάζω τις τιμές απο την λίστα metrhshes στο γράφημα
+
+        ArrayList<BarEntry> Entries = new ArrayList<>();
+
+
+        // εδω χρείάζεται να γίνει με for loop για να μπαίνουν αυτόματα όλες οι τιμές της λίστας metrhshes
+
+        float t1;
+
+        for(int i=0;i<metrhshes.size();i++)
+        {
+
+            t1= (float) metrhshes.get(i).getKilovatora();
+            Entries.add(new BarEntry(t1,i));
+        }
+
+
+
+
+
+
+        // Τοποθέτηση δεδομένων στο barchar
+
+
+        //οι δοκιμαστικές τιμές για το γράφημα
         ArrayList<BarEntry> barEntries = new ArrayList<>();
         barEntries.add(new BarEntry(44f, 0));
         barEntries.add(new BarEntry(88f, 1));
@@ -41,124 +115,31 @@ public class MainActivity extends AppCompatActivity {
         barEntries.add(new BarEntry(91f, 5));
 
 
-        BarDataSet barDataSet = new BarDataSet(barEntries, "Dates");
+        BarDataSet barDataSet = new BarDataSet(Entries, "Dates");
 
-        ArrayList<String> theDates = new ArrayList<>();
-        theDates.add("19 December");
-        theDates.add("20 December");
-        theDates.add("21 December");
-        theDates.add("22 December");
-        theDates.add("23 December");
-        theDates.add("24 December");
+        // Εδω βάζω τις τιμές απο την λίστα metrhshes στο γράφημα
+        ArrayList<String> theDays = new ArrayList<>();
+        // εδω χρείάζεται να γίνει με for loop για να μπαίνουν αυτόματα όλες οι τιμές της λίστας metrhshes
+        for(int i =0;i<metrhshes.size();i++)
+        {
+            theDays.add(metrhshes.get(i).getHmera());
+        }
 
+        // Τοποθέτηση δεδομένων στο barchar
 
-        BarData theData = new BarData(theDates, barDataSet);
+        BarData theData = new BarData(theDays, barDataSet);
         barChar.setData(theData);
+
+        //διάφορες άλλες επιλογές
         barChar.setDragEnabled(true);
         barChar.setScaleEnabled(true);
         barChar.setTouchEnabled(true);
+        barChar.setNoDataText("Description that you want");
 
 
-//        ListView listView = (ListView)findViewById(R.id.lv);
-//        //create a string array for listview
-//
-//        ArrayList<String> list = new ArrayList<String>();
-//
-//        for(int i =1;i<15;i++)
-//        {
-//            list.add("Μέτρηση υπ' αριθμόν: "+ i);
-//        }
-//
-//        //create adapter to show this array like a list
-//
-//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,list);
-//
-//        //put this adapter to listview
-//
-//        listView.setAdapter(adapter);
-
-        ArrayList<Metrhsh> metrhshes = new ArrayList<>();
-
-        metrhshes.add(new Metrhsh("13 Noemvriou", 14));
-        metrhshes.add(new Metrhsh("14 Noemvriou", 4));
-        metrhshes.add(new Metrhsh("15 Noemvriou", 3));
-        metrhshes.add(new Metrhsh("16 Noemvriou", 0.1));
-        metrhshes.add(new Metrhsh("13 Noemvriou", 14));
-        metrhshes.add(new Metrhsh("14 Noemvriou", 4));
-        metrhshes.add(new Metrhsh("15 Noemvriou", 3));
-        metrhshes.add(new Metrhsh("16 Noemvriou", 0.1));
-
-
-
-        //custom ArrayAdapter
-        class metrhshArrayAdapter extends ArrayAdapter<Metrhsh> {
-
-            private Context context;
-            private ArrayList<Metrhsh> metrhshes;
-
-            //constructor, call on creation
-            public metrhshArrayAdapter(Context context, int resource, ArrayList<Metrhsh> objects) {
-                super(context, resource, objects);
-
-                this.context = context;
-                this.metrhshes = objects;
-            }
-
-            //called when rendering the list
-            public View getView(int position, View convertView, ViewGroup parent) {
-
-                //get the property we are displaying
-                Metrhsh metrhsh = metrhshes.get(position);
-
-                //get the inflater and inflate the XML layout for each item
-                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
-                View view = inflater.inflate(R.layout.layout, null);
-
-                TextView hmeromhnia = (TextView) view.findViewById(R.id.textView1);
-                TextView kwh = (TextView) view.findViewById(R.id.textView2);
-
-                ImageView image = (ImageView) view.findViewById(R.id.imageView1);
-
-
-
-
-
-
-                //set address and description
-                String hmeromhnias = metrhsh.getHmera();
-                hmeromhnia.setText(hmeromhnias);
-
-
-                //set price and rental attributes
-                kwh.setText("Kwh: " + metrhsh.getKilovatora());
-
-                //get the image associated with this property
-
-               image.setImageResource(metrhsh.getImage());
-
-                return view;
-//
-//                View  = getLayoutInflater().inflate(R.layout.header, null);
-//                View footer = getLayoutInflater().inflate(R.layout.footer, null);
-//
-//
-//                view.addHeaderView(header);
-//                metrhshes.addFooterView(footer);
-//
-//
-//                View viewheader = View.inflate(context, R.layout.header, null);
-//                 = (ListView) findViewById(R.id.lv);
-//
-//
-//                lv1.addHeaderView(view);
-            }
-        }
-        ArrayAdapter<Metrhsh> adapter = new metrhshArrayAdapter(this,0,metrhshes);
-
-        ListView listView =(ListView) findViewById(R.id.lv);
-        listView.setAdapter(adapter);
-
+        //ένα δοκιμαστικό textView που είναι στο footer layout μαζί με το barchar
+        TextView t =(TextView) findViewById(R.id.hello);
+        t.setText("hi");
     }
-
-
 }
+
