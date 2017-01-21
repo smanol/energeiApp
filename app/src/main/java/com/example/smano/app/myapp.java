20442ee34db656016afc64f32c92f019c33c5307
Georgios.Manoliadis <georgemanoliadi@hotmail.com>
2017-01-21 12:05:12 +0200

    Adding notification library in build.gradle .

---
+++ app/src/main/java/com/example/smano/app/myapp.java
@@ -0,0 +1,20 @@
+package com.example.smano.app;
+
+import android.app.Application;
+
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.FirebaseApp;
+/**
+ * Created by Georgios.Manoliadis on 18/12/2016.
+ */
+
+public class myapp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //FirebaseApp.initializeApp(this);
+        if(!FirebaseApp.getApps(this).isEmpty()) {
+            FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+        }
+    }
+}
