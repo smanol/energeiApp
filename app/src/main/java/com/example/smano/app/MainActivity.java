1141216fbcaf511cef8903a2382face6b1271f79
smanol <smanoliadis@gmail.com>
2017-02-18 18:30:13 +0200

    Ypologismnos Anhgmenoy kostoys

--- app/src/main/java/com/example/smano/app/MainActivity.java
+++ app/src/main/java/com/example/smano/app/MainActivity.java
@@ -1,41 +1,29 @@
 package com.example.smano.app;
 
-import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Toast;
 
-import com.firebase.client.Firebase;
-import com.firebase.client.FirebaseError;
 import com.firebase.ui.auth.AuthUI;
-
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.UserInfo;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -371,7 +359,175 @@ public class MainActivity extends AppCompatActivity {
         myDB.child("Users").child(name).setValue(user);
     }
 
+    public void calculateCost(Double kilovat){
 
+        double a,b,c,d,e,f,g,h,i,j,k,l ;
+        double meresMhna = 30 ;
+        final double meresEtous = 365;
+        double anhgmenhKatanalwshMhna = kilovat * meresMhna;
+
+
+        double pagio = 1.52;
+
+        a=pagio;
+
+        double kVA = 8;
+
+        //Χρέωση Προμήθειας
+
+        double xrewshPromhthias;
+        if (kilovat*30>2000)
+        {
+            xrewshPromhthias=0.10252;
+        }
+        else {
+            xrewshPromhthias=0.0946;
+             }
+
+        double xrewshPromhthiasCost = xrewshPromhthias * anhgmenhKatanalwshMhna;
+
+        b=xrewshPromhthiasCost;
+
+        //Υπηρεσίες Κοινής Ωφέλειας
+        double YKW;
+
+        if(kilovat<1600)
+        {
+            YKW=0.00699;
+        }
+        else if(1601<kilovat && kilovat<2000)
+        {
+            YKW=0.0157;
+        }
+        else if(2001<kilovat && kilovat<3000)
+        {
+            YKW=0.03987;
+        }
+
+        else {
+            YKW=0.04488;
+        }
+
+        double YKWCost= YKW * anhgmenhKatanalwshMhna;
+        c = YKWCost;
+        //Σύστημα Μεταφοράς
+
+        double isxysSysthmatosMetaforas = 0.13;
+
+        double isxysSysthmatosMetaforasCost = isxysSysthmatosMetaforas * kVA * meresMhna / meresEtous ;
+        d = isxysSysthmatosMetaforasCost;
+
+
+        double energeiaSysthmatosMetaforas = 0.00527;
+
+        double energeiaSysthmatosMetaforasCost = energeiaSysthmatosMetaforas * anhgmenhKatanalwshMhna;
+        e = energeiaSysthmatosMetaforasCost;
+
+        //Δίκτυο Μεταφοράς
+
+        double isxysDiktyouDianomhs = 0.54;
+        double isxysDiktyouDianomhsCost = isxysDiktyouDianomhs * kVA * meresMhna / meresEtous ;
+
+        f= isxysDiktyouDianomhsCost;
+
+        double energeiaDiktyouDianomhs = 0.0213;
+        double energeiaDiktyouDianomhsCost = energeiaSysthmatosMetaforas * anhgmenhKatanalwshMhna;
+        g = energeiaDiktyouDianomhsCost;
+        //Λοιπές Χρεώσεις
+
+        double loipesXrewseis = 0.00007 ;
+        double loipesXrewseisCost = loipesXrewseis *anhgmenhKatanalwshMhna;
+        h =loipesXrewseisCost;
+
+        //Ειδικό Τέλος Μείωσης Εκπομπών Αερίων Ρύπων
+
+        double ETMEAR = 0.02477;
+
+        double ETMEARCost = ETMEAR * anhgmenhKatanalwshMhna;
+
+        i = ETMEARCost;
+
+        //Ειδικός Φόρος Κατανάλωσης
+
+        double EFK = 0.0022;
+
+        double EFKCost = EFK * anhgmenhKatanalwshMhna;
+
+        k = EFKCost;
+
+        //Ειδικό Τέλος
+
+        double eidikoTelos = 0.0005;
+        double eidikoTelosCost = eidikoTelos * (a+b+c+d+f+g+h+i-k);
+
+        l=eidikoTelosCost;
+        //ΦΠΑ
+        double fpa = 0.13;
+        double fpaCost = fpa *(a+b+c+d+f+g+h+k);
+        j=fpaCost;
+
+
+
+        double posoPlerwmhs = a+b+c+d+f+g+h+i+k+l+j;
+
+    }
+
+
+
+    private void calculateCostNight(double kilovatorNight){
+
+        //Βοηθητικές Μεταβλητές
+
+        double an,bn,cn,dn,en,fn,hn,in,jn;
+
+        // Ανηγμένη Κατανάλωση Μήνα
+        double anhgmenhKatanalwshMhnaNight = kilovatorNight*30 ;
+        // Πάγιο Νύχτα
+        double pagioNight = 2.00;
+        an=pagioNight;
+        //Χρέωση Προμήθειας Νύχτας
+        double xrewshPromhthiasNight = 0.06610;
+        double xrewshPromhthiasNightCost= xrewshPromhthiasNight * anhgmenhKatanalwshMhnaNight;
+        bn=xrewshPromhthiasNightCost;
+        //Υπηρεσίες Κοινής Οφέλειας Χρέωση Νύχτας
+        double YKWNight = 0.00889;
+        double YKWNightCost= YKWNight * anhgmenhKatanalwshMhnaNight;
+        cn = YKWNightCost;
+
+
+
+        //Λοιπές Χρεώσεις
+
+        double loipesXrewseisNight = 0.00007 ;
+        double loipesXrewseisCostNight = loipesXrewseisNight *anhgmenhKatanalwshMhnaNight;
+        hn =loipesXrewseisCostNight;
+
+
+        //Ειδικό Τέλος Μείωσης Εκπομπών Αεων Ρύπων Χρέωση Νύχτας
+        double ETMEARNight = 0.02477;
+
+        double ETMEARCostNight = ETMEARNight * anhgmenhKatanalwshMhnaNight;
+
+        in = ETMEARCostNight;
+
+        //Ειδικός Φόρος Κατανάλωσης Χρέωση Νύχτας
+
+        double EFKNight = 0.0022;
+
+        double EFKCostNihgt = EFKNight* anhgmenhKatanalwshMhnaNight;
+
+        jn = EFKCostNihgt;
+
+        //Ειδικό Τέλος
+        double eidikoTelos= 0.005;
+        double eidikoTelosCostNight= eidikoTelos*(jn-in+hn+ an +bn+cn)
+        en=eidikoTelosCostNight;
+        // ΦΠΑ: (Χρεώσεις Προμήθειας ΔΕΗ + Ρυθμιζόμενες + ΕΦΚ) x 13%
+        double FPA = 0.13;
+        double FPACostNight = FPA * (jn+in+hn+ an +bn+cn );
+        dn=FPACostNight;
+        double totalCost=  an+bn+cn+dn+en+jn+in+hn;
+    }
 
     //TODO: implement a function that will translate the kilovatores to KilovatoresDifferences
 }
